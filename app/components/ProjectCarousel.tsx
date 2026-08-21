724281e77790cf738b287d0b1085f98850a014ab
riyu.iring <oribelloruthiemy@gmail.com>
2026-08-22 07:41:25 +0800

    Add trackpad swiping...

--- app/components/ProjectCarousel.tsx
+++ app/components/ProjectCarousel.tsx
@@ -2,13 +2,14 @@
 import useEmblaCarousel from 'embla-carousel-react'
 import { Card } from './Card'
 import { featuredProjects } from '../data/projects'
+import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures'
 
 export const ProjectCarousel = () => {
     const [emblaRef] = useEmblaCarousel({
         align: 'start',
         containScroll: 'trimSnaps',
         dragFree: true,
-    })
+    }, [WheelGesturesPlugin()])
 
     return (
         <div className="overflow-hidden w-[calc(100vw-3rem)] text-sm pt-0" ref={emblaRef}>
