08de90935a22845db75956361d71f0f8bb7ddac0
riyu.iring <oribelloruthiemy@gmail.com>
2026-08-26 04:58:55 +0800

    Update...

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,25 +1,20 @@
 'use client'
-import { Suspense } from "react"
-import { useSearchParams } from "next/navigation"
 import { PageLayout } from "../components/PageLayout"
 import { PageHeaders } from "../components/PageHeaders"
 import { ContactForm } from "../components/ContactForm";
+import {use} from 'react'
 
-function ContactContent() {
-    const searchParams = useSearchParams();
-    const emailType = searchParams.get("emailType") || "general";
-    
-    return <ContactForm emailType={emailType}/>
-}
-
-export default function Contact() {
+export default function Contact({
+    searchParams
+} : {
+    searchParams: Promise<{emailType: "general" | "freelance"}>
+}) {
+    const {emailType = "general" } = use(searchParams)
     return (
         <PageLayout
             headers={<PageHeaders title="Feel free to send me an email" subtitle="I typically respond within 24–48 hours." />}
             children={
-                <Suspense fallback={<div className="animate-pulse bg-zinc-800/50 rounded-xl h-[400px]"></div>}>
-                    <ContactContent />
-                </Suspense>
+                <ContactForm emailType={emailType}/>
             }
         />
     )
