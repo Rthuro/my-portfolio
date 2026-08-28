bcbbbeae4a2777e2404f4c2b2ad5dbc6984db8ef
riyu.iring <oribelloruthiemy@gmail.com>
2026-08-28 19:34:59 +0800

    update analytics UI

--- app/components/PortfolioAnalytics.tsx
+++ app/components/PortfolioAnalytics.tsx
@@ -22,21 +22,6 @@ export function PortfolioAnalytics({ type }: Type) {
     return (
         <>
             {type === 'visitors' && (
-                <div className="flex flex-wrap items-center justify-center gap-3 bg-zinc-200 border border-zinc-800 p-4 md:p-5 rounded-3xl">
-                    <div className="flex gap-0.5">
-                        {(visit > 0) && memoji.slice(0, 3).map((mem, idx) => (
-                            <img key={idx} src={mem.src} alt="mem" className={`size-10 md:size-12 object-center rounded-full p-1 border-2 border-white bg-zinc-100 ${idx == 0 || idx == 2 ? "mt-3" : ""} `} />
-
-                        ))}
-                    </div>
-
-                    <p className=" text-zinc-700 text-sm text-center">
-                        <span className="text-zinc-900 font-bold mr-1">{visit ?? "..."}</span> total visits
-                    </p>
-                </div>
-            )}
-
-            {type === 'current-visitors' && (
                 <div className="flex flex-col items-center justify-center gap-3 bg-zinc-900 border border-zinc-800 p-4 md:p-5  rounded-3xl">
                     <div className="flex items-end">
                         <div className="flex items-center">
@@ -53,7 +38,23 @@ export function PortfolioAnalytics({ type }: Type) {
 
                     </div>
                     <p className=" text-zinc-200 text-sm text-center">
-                        <span className="text-white font-bold mr-1">{visit ?? "..."}</span> people viewed today
+                        <span className="text-white font-bold mr-1">{visit ?? "..."}</span> total visits
+                    </p>
+                </div>
+                
+            )}
+
+            {type === 'current-visitors' && (
+                <div className="flex flex-wrap items-center justify-center gap-3 bg-zinc-200 border border-zinc-800 p-4 md:p-5 rounded-3xl">
+                    <div className="flex gap-0.5">
+                        {(visit > 0) && memoji.slice(0, 3).map((mem, idx) => (
+                            <img key={idx} src={mem.src} alt="mem" className={`size-10 md:size-12 object-center rounded-full p-1 border-2 border-white bg-zinc-100 ${idx == 0 || idx == 2 ? "mt-3" : ""} `} />
+
+                        ))}
+                    </div>
+
+                    <p className=" text-zinc-700 text-sm text-center">
+                        <span className="text-zinc-900 font-bold mr-1">{visit ?? "..."}</span> people viewed today
                     </p>
                 </div>
             )}
